0c69d1104e0809350693b819cb882eeaa4ecff9b
omcewan <orlandomcewan231@gmail.com>
2021-12-25 12:56:45 -0500

    add individual task through use of the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@ var tasksToDoEL = document.querySelector("#task-to-do");
 
 var createTaskHandler = function(){
     event.preventDefault();
+    var taskInputName = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new Task";
+
+    // create div to hold task info and add to a list
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    
+    // add HTML info to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskInputName + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEL.appendChild(listItemEl);
 };
 
