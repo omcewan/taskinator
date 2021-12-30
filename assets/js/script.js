bb1f09b388b71da01b8654b27eefb92cb0967091
omcewan <orlandomcewan231@gmail.com>
2021-12-30 00:37:37 -0500

    add ability to delete task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#task-to-do");
@@ -90,3 +91,18 @@ var createTaskActions = function(taskId){
 }
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event){
+
+    if(event.target.matches(".delete-btn")){
+        // get the elements task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+}
+
+var deleteTask = function(taskId){
+    var taskSelected = document.querySelector(".task-item[data-task-id ='"+ taskId + "']");
+    taskSelected.remove();
+}
+pageContentEl.addEventListener("click", taskButtonHandler); 
